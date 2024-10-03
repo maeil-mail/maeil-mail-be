b453645546e3f64196ecf835cf3c4ac1eadd05ba
le2sky <leehaneul990623@gmail.com>
2024-10-03 22:04:03 +0900

    refactor: 구독자 인증 서비스 분리

--- src/main/java/maeilmail/subscribe/core/SubscribeQuestionService.java
+++ src/main/java/maeilmail/subscribe/core/SubscribeQuestionService.java
@@ -1,11 +1,7 @@
 package maeilmail.subscribe.core;
 
-import java.util.HashMap;
-import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import maeilmail.mail.MailMessage;
-import maeilmail.mail.MailSender;
 import maeilmail.question.QuestionCategory;
 import maeilmail.subscribe.core.request.SubscribeQuestionRequest;
 import maeilmail.subscribe.core.request.VerifyEmailRequest;
@@ -18,28 +14,13 @@ import org.springframework.transaction.annotation.Transactional;
 public class SubscribeQuestionService {
 
     private final SubscribeRepository subscribeRepository;
-    private final MailSender mailSender;
-    private final VerifyMailView verifyEmailView;
-    private final CodeGenerator codeGenerator;
-    private final TemporalSubscriberStore temporalSubscriberStore;
-
-    public void sendCodeIncludedMail(VerifyEmailRequest request) {
-        String subject = "이메일 인증을 진행해주세요.";
-        String code = codeGenerator.generateCode();
-        String text = createText(code);
-        MailMessage mailMessage = new MailMessage(request.email(), subject, text, verifyEmailView.getType());
-
-        log.info("인증 코드 포함 메일 요청, 이메일 = {} 코드 = {}", request.email(), code);
-        mailSender.sendMail(mailMessage);
-
-        temporalSubscriberStore.add(request.email(), code);
-    }
+    private final SubscribeVerifyService subscribeVerifyService;
 
     @Transactional
     public void subscribe(SubscribeQuestionRequest request) {
         log.info("이메일 구독 요청, 이메일 = {}", request.email());
 
-        temporalSubscriberStore.verify(request.email(), request.code());
+        subscribeVerifyService.verify(request.email(), request.code());
         QuestionCategory category = QuestionCategory.from(request.category());
         Subscribe subscribe = new Subscribe(request.email(), category);
 
@@ -47,10 +28,7 @@ public class SubscribeQuestionService {
         subscribeRepository.save(subscribe);
     }
 
-    private String createText(String code) {
-        Map<Object, Object> attribute = new HashMap<>();
-        attribute.put("code", code);
-
-        return verifyEmailView.render(attribute);
+    public void sendCodeIncludedMail(VerifyEmailRequest request) {
+        subscribeVerifyService.sendCodeIncludedMail(request);
     }
 }
---
+++ src/main/java/maeilmail/subscribe/core/SubscribeVerifyService.java
@@ -0,0 +1,44 @@
+package maeilmail.subscribe.core;
+
+import java.util.HashMap;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import maeilmail.mail.MailMessage;
+import maeilmail.mail.MailSender;
+import maeilmail.subscribe.core.request.VerifyEmailRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+class SubscribeVerifyService {
+
+    private final CodeGenerator codeGenerator;
+    private final VerifyMailView verifyMailView;
+    private final MailSender mailSender;
+    private final TemporalSubscriberStore temporalSubscriberStore;
+
+    public void sendCodeIncludedMail(VerifyEmailRequest request) {
+        String subject = "이메일 인증을 진행해주세요.";
+        String code = codeGenerator.generateCode();
+        String text = createText(code);
+        MailMessage mailMessage = new MailMessage(request.email(), subject, text, verifyMailView.getType());
+
+        log.info("인증 코드 포함 메일 요청, 이메일 = {} 코드 = {}", request.email(), code);
+        mailSender.sendMail(mailMessage);
+
+        temporalSubscriberStore.add(request.email(), code);
+    }
+
+    private String createText(String code) {
+        Map<Object, Object> attribute = new HashMap<>();
+        attribute.put("code", code);
+
+        return verifyMailView.render(attribute);
+    }
+
+    public void verify(String email, String code) {
+        temporalSubscriberStore.verify(email, code);
+    }
+}
