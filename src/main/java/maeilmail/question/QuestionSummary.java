fd1dfbcfd13279f467b2cbfc46ab2d7029f81f60
le2sky <leehaneul990623@gmail.com>
2024-10-03 23:19:09 +0900

    refactor: 미사용 생성자 제거

--- src/main/java/maeilmail/question/QuestionSummary.java
+++ src/main/java/maeilmail/question/QuestionSummary.java
@@ -4,10 +4,6 @@ import com.querydsl.core.annotations.QueryProjection;
 
 public record QuestionSummary(Long id, String title, String content, String category) {
 
-    public QuestionSummary(Question question) {
-        this(question.getId(), question.getTitle(), question.getContent(), question.getCategory().toLowerCase());
-    }
-
     @QueryProjection
     public QuestionSummary {
     }
