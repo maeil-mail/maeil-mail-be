c0089395fd7f3bc3f00c9f35d52d190b28f65e0f
le2sky <leehaneul990623@gmail.com>
2024-10-03 21:33:27 +0900

    fix: ddl 업데이트

--- src/main/resources/ddl.sql
+++ src/main/resources/ddl.sql
@@ -1,7 +1,7 @@
 create table question
 (
     id       bigint auto_increment,
-    content  text not null,
+    content  text         not null,
     title    varchar(255) not null,
     category enum ('BACKEND','FRONTEND') not null,
     primary key (id)
@@ -12,5 +12,22 @@ create table subscribe
     id       bigint auto_increment,
     email    varchar(255) not null,
     category enum ('BACKEND','FRONTEND') not null,
+    primary key (id)
+);
+
+create table mail_event
+(
+    id         bigint auto_increment,
+    date       date,
+    is_success boolean      not null,
+    email      varchar(255) not null,
+    type       varchar(255) not null,
+    primary key (id)
+);
+
+create table admin
+(
+    id    bigint auto_increment,
+    email varchar(255) not null,
     primary key (id)
 );
