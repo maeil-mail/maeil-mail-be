da4a9d1a5385ab1f88beb64d401d2cedf6bc4649
le2sky <leehaneul990623@gmail.com>
2024-10-14 21:55:57 +0900

    fix: 비동기 스레드풀 설정 변경

--- src/main/java/maeilmail/AsyncConfig.java
+++ src/main/java/maeilmail/AsyncConfig.java
@@ -1,6 +1,5 @@
 package maeilmail;
 
-import java.util.concurrent.Executor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
@@ -11,12 +10,17 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 class AsyncConfig implements AsyncConfigurer {
 
+    /**
+     * corePoolSize보다 적은 스레드가 있거나 maxPoolSize보다 적은 스레드가 실행중이고,
+     * queueCapacity에 의해 정의된 큐 사이즈가 가득찰 경우 스레드가 생성된다.
+     */
     @Override
     @Bean("mailExecutor")
-    public Executor getAsyncExecutor() {
+    public ThreadPoolTaskExecutor getAsyncExecutor() {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
         threadPoolTaskExecutor.setCorePoolSize(1);
-        threadPoolTaskExecutor.setMaxPoolSize(5);
+        threadPoolTaskExecutor.setQueueCapacity(2);
+        threadPoolTaskExecutor.setMaxPoolSize(10);
         threadPoolTaskExecutor.setThreadNamePrefix("async mail executor-");
         threadPoolTaskExecutor.initialize();
 
