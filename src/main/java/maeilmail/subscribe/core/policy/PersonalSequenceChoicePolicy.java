b09632bda77f9dab778fa28fe83df1a3084daca4
le2sky <leehaneul990623@gmail.com>
2024-10-20 19:31:58 +0900

    feat: 사용자별 순차 전송 기능 구현

---
+++ src/main/java/maeilmail/subscribe/core/policy/PersonalSequenceChoicePolicy.java
@@ -0,0 +1,66 @@
+package maeilmail.subscribe.core.policy;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import maeilmail.question.QuestionCategory;
+import maeilmail.question.QuestionQueryService;
+import maeilmail.question.QuestionSummary;
+import maeilmail.subscribe.core.ChoiceQuestionPolicy;
+import maeilmail.subscribe.core.Subscribe;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+class PersonalSequenceChoicePolicy implements ChoiceQuestionPolicy {
+
+    private final LocalDate backendDefaultDate = LocalDate.of(2024, 10, 16);
+    private final LocalDate frontendDefaultDate = LocalDate.of(2024, 10, 14);
+    private final QuestionQueryService questionQueryService;
+
+    @Override
+    public QuestionSummary choice(Subscribe subscribe, LocalDate today) {
+        LocalDate subscribeDate = getOrDefaultSubscribeDate(subscribe.getSubscribeDate(), subscribe.getCategory());
+        validateInvalidDate(subscribeDate, today);
+        List<QuestionSummary> questions = findQuestions(subscribe);
+        Period period = Period.between(subscribeDate, today);
+
+        return questions.get(period.getDays() % questions.size());
+    }
+
+    private LocalDate getOrDefaultSubscribeDate(LocalDate subscribeDate, QuestionCategory category) {
+        if (subscribeDate == null) {
+            return getDefaultSubscribeDate(category);
+        }
+
+        return subscribeDate;
+    }
+
+    private LocalDate getDefaultSubscribeDate(QuestionCategory category) {
+        if (category.equals(QuestionCategory.BACKEND)) {
+            return backendDefaultDate;
+        }
+
+        return frontendDefaultDate;
+    }
+
+    private void validateInvalidDate(LocalDate subscribeDate, LocalDate today) {
+        if (subscribeDate.isAfter(today)) {
+            throw new IllegalArgumentException("질문지를 결정할 수 없습니다.");
+        }
+    }
+
+    private List<QuestionSummary> findQuestions(Subscribe subscribe) {
+        List<QuestionSummary> questions = questionQueryService.queryAllByCategory(subscribe.getCategory().name());
+        validateQuestionEmpty(questions);
+
+        return questions;
+    }
+
+    private void validateQuestionEmpty(List<QuestionSummary> questions) {
+        if (questions.isEmpty()) {
+            throw new IllegalStateException("질문지를 결정할 수 없습니다.");
+        }
+    }
+}
---
+++ src/test/java/maeilmail/subscribe/core/policy/PersonalSequenceChoicePolicyTest.java
@@ -0,0 +1,164 @@
+package maeilmail.subscribe.core.policy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import maeilmail.question.Question;
+import maeilmail.question.QuestionCategory;
+import maeilmail.question.QuestionRepository;
+import maeilmail.question.QuestionSummary;
+import maeilmail.subscribe.core.Subscribe;
+import maeilmail.subscribe.core.SubscribeRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class PersonalSequenceChoicePolicyTest {
+
+    @Autowired
+    private SubscribeRepository subscribeRepository;
+
+    @Autowired
+    private QuestionRepository questionRepository;
+
+    @Autowired
+    private PersonalSequenceChoicePolicy personalSequenceChoicePolicy;
+
+    @Test
+    @DisplayName("구독일을 이용해 사용자별 순차 방식으로 질문지를 선택한다.")
+    void choiceWithSubscribedAt() {
+        LocalDate today = LocalDate.now();
+        Subscribe subscribe = createSubscribe(QuestionCategory.BACKEND);
+        createQuestion("질문1", QuestionCategory.BACKEND);
+        createQuestion("질문2", QuestionCategory.BACKEND);
+
+        QuestionSummary choice = personalSequenceChoicePolicy.choice(subscribe, today);
+
+        assertThat(choice.title()).isEqualTo("질문1");
+    }
+
+    @Test
+    @DisplayName("구독일이 주어진 날짜보다 클 수 없다.")
+    void cantChoice() {
+        LocalDate yesterday = LocalDate.now().minusDays(1);
+        Subscribe subscribe = createSubscribe(QuestionCategory.BACKEND);
+
+        assertThatThrownBy(() -> personalSequenceChoicePolicy.choice(subscribe, yesterday))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("질문지를 결정할 수 없습니다.");
+    }
+
+    @Test
+    @DisplayName("질문이 존재하지 않으면 질문지를 선택할 수 없다.")
+    void cantChoice2() {
+        LocalDate today = LocalDate.now();
+        Subscribe subscribe = createSubscribe(QuestionCategory.BACKEND);
+
+        assertThatThrownBy(() -> personalSequenceChoicePolicy.choice(subscribe, today))
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("질문지를 결정할 수 없습니다.");
+    }
+
+    /**
+     * 24년 10월 21일부로 해당 기능이 적용되어야하는데, 10월 20일에 백엔드 5번 질문지를 발송됐다.
+     * 24년 10월 20일자 기준, 기존 사용자들의 구독일이 null이다.
+     * Period(2024-10-16, 2024-10-21).getDays() + 1 = 6번 질문지를 발송할 수 있으므로,
+     * 백엔드의 구독일이 존재하지 않는 경우, 구독일을 10월 16일로 판단하여 기존 사용자가 받던 순서대로 질문을 선택한다.
+     */
+    @Test
+    @DisplayName("백엔드 구독자가 구독일이 존재하지 않는 경우, 구독일을 10월 16일로 판단한다.")
+    void backendDefaultChoice() {
+        Subscribe subscribe = mock(Subscribe.class);
+        when(subscribe.getCategory())
+                .thenReturn(QuestionCategory.BACKEND);
+        when(subscribe.getSubscribeDate())
+                .thenReturn(null);
+        createQuestions(10, QuestionCategory.BACKEND);
+
+        QuestionSummary choice1 = personalSequenceChoicePolicy.choice(subscribe, LocalDate.of(2024, 10, 20));
+        QuestionSummary choice2 = personalSequenceChoicePolicy.choice(subscribe, LocalDate.of(2024, 10, 21));
+        QuestionSummary choice3 = personalSequenceChoicePolicy.choice(subscribe, LocalDate.of(2024, 10, 22));
+
+        assertThat(choice1.title()).isEqualTo("질문5");
+        assertThat(choice2.title()).isEqualTo("질문6");
+        assertThat(choice3.title()).isEqualTo("질문7");
+    }
+
+    /**
+     * 24년 10월 21일부로 해당 기능이 적용되어야하는데, 10월 20일에 프론트엔드 7번 질문지를 발송됐다.
+     * 24년 10월 20일자 기준, 기존 사용자들의 구독일이 null이다.
+     * Period(2024-10-14, 2024-10-21).getDays() + 1 = 8번 질문지를 발송할 수 있으므로,
+     * 프론트엔드의 구독일이 존재하지 않는 경우, 구독일을 10월 14일로 판단하여 기존 사용자가 받던 순서대로 질문을 선택한다.
+     */
+    @Test
+    @DisplayName("프론트엔드 구독자가 구독일이 존재하지 않는 경우, 구독일을 10월 14일로 판단한다.")
+    void frontendDefaultChoice() {
+        Subscribe subscribe = mock(Subscribe.class);
+        when(subscribe.getCategory())
+                .thenReturn(QuestionCategory.FRONTEND);
+        when(subscribe.getSubscribeDate())
+                .thenReturn(null);
+        createQuestions(10, QuestionCategory.FRONTEND);
+
+        QuestionSummary choice1 = personalSequenceChoicePolicy.choice(subscribe, LocalDate.of(2024, 10, 20));
+        QuestionSummary choice2 = personalSequenceChoicePolicy.choice(subscribe, LocalDate.of(2024, 10, 21));
+        QuestionSummary choice3 = personalSequenceChoicePolicy.choice(subscribe, LocalDate.of(2024, 10, 22));
+
+        assertThat(choice1.title()).isEqualTo("질문7");
+        assertThat(choice2.title()).isEqualTo("질문8");
+        assertThat(choice3.title()).isEqualTo("질문9");
+    }
+
+    @Test
+    @DisplayName("질문지를 결정한다.")
+    void choice() {
+        Subscribe subscribe = createSubscribe(QuestionCategory.BACKEND);
+        LocalDate today = LocalDate.now();
+
+        createQuestion("질문1", QuestionCategory.BACKEND);
+        createQuestion("질문2", QuestionCategory.BACKEND);
+        createQuestion("질문3", QuestionCategory.BACKEND);
+        createQuestion("질문4", QuestionCategory.FRONTEND);
+        createQuestion("질문5", QuestionCategory.BACKEND);
+        createQuestion("질문6", QuestionCategory.BACKEND);
+
+        QuestionSummary choice1 = personalSequenceChoicePolicy.choice(subscribe, today);
+        QuestionSummary choice2 = personalSequenceChoicePolicy.choice(subscribe, today.plusDays(1));
+        QuestionSummary choice3 = personalSequenceChoicePolicy.choice(subscribe, today.plusDays(2));
+        QuestionSummary choice4 = personalSequenceChoicePolicy.choice(subscribe, today.plusDays(3));
+        QuestionSummary choice5 = personalSequenceChoicePolicy.choice(subscribe, today.plusDays(4));
+        QuestionSummary choice6 = personalSequenceChoicePolicy.choice(subscribe, today.plusDays(5));
+
+        assertThat(choice1.title()).isEqualTo("질문1");
+        assertThat(choice2.title()).isEqualTo("질문2");
+        assertThat(choice3.title()).isEqualTo("질문3");
+        assertThat(choice4.title()).isEqualTo("질문5");
+        assertThat(choice5.title()).isEqualTo("질문6");
+        assertThat(choice6.title()).isEqualTo("질문1");
+    }
+
+    private void createQuestions(int size, QuestionCategory questionCategory) {
+        for (int i = 1; i <= size; i++) {
+            createQuestion("질문" + i, questionCategory);
+        }
+    }
+
+    private void createQuestion(String questionTitle, QuestionCategory category) {
+        Question question = new Question(questionTitle, "content", category);
+
+        questionRepository.save(question);
+    }
+
+    private Subscribe createSubscribe(QuestionCategory category) {
+        Subscribe subscribe = new Subscribe("lee@gmail.com", category);
+
+        return subscribeRepository.save(subscribe);
+    }
+}
