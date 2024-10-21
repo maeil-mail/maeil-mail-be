4f4d1221d657f24b4296bc4ed8358c00bffc1fc4
le2sky <leehaneul990623@gmail.com>
2024-10-21 19:44:44 +0900

    feat: 전체 질문 조회 api 제거

--- src/main/java/maeilmail/question/QuestionApi.java
+++ src/main/java/maeilmail/question/QuestionApi.java
@@ -1,11 +1,9 @@
 package maeilmail.question;
 
-import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -13,15 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 class QuestionApi {
 
     private final QuestionQueryService questionQueryService;
-
-    @GetMapping("/question")
-    public ResponseEntity<List<QuestionSummary>> getQuestions(
-            @RequestParam(defaultValue = "all") String category
-    ) {
-        List<QuestionSummary> summaries = questionQueryService.queryAllByCategory(category);
-
-        return ResponseEntity.ok(summaries);
-    }
 
     @GetMapping("/question/{id}")
     public ResponseEntity<QuestionSummary> getQuestionById(@PathVariable Long id) {
