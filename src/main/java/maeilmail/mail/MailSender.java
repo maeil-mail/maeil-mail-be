abeae496a59166d2f103041b1c2df6702fde4301
kunsanglee <gosmdochee2@gmail.com>
2024-10-28 16:28:55 +0900

    refactor: MimeMessage 생성하는 로직 메서드 추출

--- src/main/java/maeilmail/mail/MailSender.java
+++ src/main/java/maeilmail/mail/MailSender.java
@@ -21,22 +21,27 @@ public class MailSender {
 
     @Async
     public void sendMail(MailMessage message) {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
 
         try {
             log.info("메일을 전송합니다. email = {} question = {} type = {}", message.to(), message.subject(), message.type());
-            tryAppendOpenEventTrace(message, mimeMessage);
-            MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-            mimeMessageHelper.setFrom(FROM_EMAIL);
-            mimeMessageHelper.setTo(message.to());
-            mimeMessageHelper.setSubject(message.subject());
-            mimeMessageHelper.setText(message.text(), true);
+            MimeMessage mimeMessage = convertToMime(message);
             javaMailSender.send(mimeMessage);
             mailEventRepository.save(MailEvent.success(message.to(), message.type()));
         } catch (MessagingException e) {
             mailEventRepository.save(MailEvent.fail(message.to(), message.type()));
             log.info("메일 전송 실패 = {}", e.getMessage());
         }
+    }
+
+    private MimeMessage convertToMime(MailMessage message) throws MessagingException {
+        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
+        tryAppendOpenEventTrace(message, mimeMessage);
+        MimeMessageHelper mimeMessageHelper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
+        mimeMessageHelper.setFrom(FROM_EMAIL);
+        mimeMessageHelper.setTo(message.to());
+        mimeMessageHelper.setSubject(message.subject());
+        mimeMessageHelper.setText(message.text(), true);
+        return mimeMessage;
     }
 
     private void tryAppendOpenEventTrace(MailMessage message, MimeMessage mimeMessage) throws MessagingException {
