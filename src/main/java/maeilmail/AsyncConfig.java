736bdbca8dae5b761c91f8141702c4c9f973e792
le2sky <leehaneul990623@gmail.com>
2024-10-30 20:23:55 +0900

    fix: 포화정책 max 풀 사이즈 제거

--- src/main/java/maeilmail/AsyncConfig.java
+++ src/main/java/maeilmail/AsyncConfig.java
@@ -1,7 +1,6 @@
 package maeilmail;
 
 import java.util.concurrent.Executor;
-import java.util.concurrent.ThreadPoolExecutor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
@@ -15,15 +14,12 @@ class AsyncConfig implements AsyncConfigurer {
     /**
      * corePoolSize보다 적은 스레드가 있거나 maxPoolSize보다 적은 스레드가 실행중이고,
      * queueCapacity에 의해 정의된 큐 사이즈가 가득찰 경우 스레드가 생성된다.
-     * CallerRunsPolicy shutdown 상태가 아닌 경우, ThreadPoolExecutor에 요청한 스레드에서 직접 처리한다.
      */
     @Override
     @Bean("mailExecutor")
     public Executor getAsyncExecutor() {
         ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
         threadPoolTaskExecutor.setCorePoolSize(3);
-        threadPoolTaskExecutor.setMaxPoolSize(10);
-        threadPoolTaskExecutor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
         threadPoolTaskExecutor.setThreadNamePrefix("async mail executor-");
         threadPoolTaskExecutor.initialize();
 
