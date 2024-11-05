3397b894a5ae59c985594bba7f39309db8efee87
le2sky <leehaneul990623@gmail.com>
2024-11-05 17:23:07 +0900

    feat: 일일 구독자 통계 API 구현

--- src/main/java/maeilmail/statistics/StatisticsApi.java
+++ src/main/java/maeilmail/statistics/StatisticsApi.java
@@ -1,4 +1,20 @@
 package maeilmail.statistics;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
 class StatisticsApi {
+
+    private final StatisticsService statisticsService;
+
+    @GetMapping("/statistics/subscribe")
+    public ResponseEntity<SubscribeReport> getDailySubscribeReport() {
+        SubscribeReport report = statisticsService.generateDailySubscribeReport();
+
+        return ResponseEntity.ok(report);
+    }
 }
