ec6aa92a17e634322a63fe6ebea668d147edf76b
le2sky <leehaneul990623@gmail.com>
2024-11-07 16:54:31 +0900

    feat: 1대의 서버에서만 시퀀스 증가 스케줄을 사용하도록 distributedSupport 적용

--- src/main/java/maeilmail/subscribe/core/SubscribeSequenceScheduler.java
+++ src/main/java/maeilmail/subscribe/core/SubscribeSequenceScheduler.java
@@ -7,6 +7,7 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import maeilmail.DistributedSupport;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,11 +20,14 @@ public class SubscribeSequenceScheduler {
     private static final ZoneId KOREA_ZONE = ZoneId.of("Asia/Seoul");
 
     private final SubscribeRepository subscribeRepository;
+    private final DistributedSupport distributedSupport;
 
     @Transactional
     @Scheduled(cron = "0 0 22 * * MON-FRI", zone = "Asia/Seoul")
     public void increaseNextQuestionSequence() {
-        LocalDateTime baseDateTime = ZonedDateTime.of(LocalDate.now(), LocalTime.of(7, 0), KOREA_ZONE).toLocalDateTime();
-        subscribeRepository.increaseNextQuestionSequence(baseDateTime);
+        if (distributedSupport.isMine(1L)) {
+            LocalDateTime baseDateTime = ZonedDateTime.of(LocalDate.now(), LocalTime.of(7, 0), KOREA_ZONE).toLocalDateTime();
+            subscribeRepository.increaseNextQuestionSequence(baseDateTime);
+        }
     }
 }
