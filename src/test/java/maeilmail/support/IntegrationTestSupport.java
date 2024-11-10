d84bccbf0898c9ab902935e589ccd7e6d0940d93
le2sky <leehaneul990623@gmail.com>
2024-11-10 18:28:28 +0900

    test: 테스트용 쿼리 카운트 유틸 구현

--- src/test/java/maeilmail/support/IntegrationTestSupport.java
+++ src/test/java/maeilmail/support/IntegrationTestSupport.java
@@ -5,13 +5,17 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 
 import maeilmail.mail.MailSender;
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
 @Transactional
+@Import(IntegrationTestSupport.TestConfig.class)
 public abstract class IntegrationTestSupport {
 
     @TestConfiguration
@@ -25,6 +29,19 @@ public abstract class IntegrationTestSupport {
                     .sendMail(any());
 
             return mailSender;
+        }
+
+        @Bean
+        public QueryCountTester queryCountTester() {
+            return new QueryCountTester();
+        }
+
+        @Bean
+        public HibernatePropertiesCustomizer hibernatePropertiesCustomizer() {
+            QueryCountInspector queryCountInspector = new QueryCountInspector(queryCountTester());
+
+            return hibernateProperties ->
+                    hibernateProperties.put(AvailableSettings.STATEMENT_INSPECTOR, queryCountInspector);
         }
     }
 }
---
+++ src/test/java/maeilmail/support/QueryCountInspector.java
@@ -0,0 +1,19 @@
+package maeilmail.support;
+
+import org.hibernate.resource.jdbc.spi.StatementInspector;
+
+class QueryCountInspector implements StatementInspector {
+
+    private final QueryCountTester queryCountTester;
+
+    public QueryCountInspector(QueryCountTester queryCountTester) {
+        this.queryCountTester = queryCountTester;
+    }
+
+    @Override
+    public String inspect(String sql) {
+        queryCountTester.tryIncrease(sql);
+
+        return sql;
+    }
+}
