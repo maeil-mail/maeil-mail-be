dbcd4fe5de55e27211067e26f3cdd2ed0e6fb9ef
le2sky <leehaneul990623@gmail.com>
2024-11-10 15:01:27 +0900

    feat: IntegrationTestSupport 구현

---
+++ src/test/java/maeilmail/support/IntegrationTestSupport.java
@@ -0,0 +1,30 @@
+package maeilmail.support;
+
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.mock;
+
+import maeilmail.mail.MailSender;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class IntegrationTestSupport {
+
+    @TestConfiguration
+    public static class TestConfig {
+
+        @Bean
+        public MailSender mailSender() {
+            MailSender mailSender = mock(MailSender.class);
+            doNothing()
+                    .when(mailSender)
+                    .sendMail(any());
+
+            return mailSender;
+        }
+    }
+}
