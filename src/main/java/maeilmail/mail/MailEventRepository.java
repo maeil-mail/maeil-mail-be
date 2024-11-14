56c62c1090583cbfa0ff29e3fa285cb459890929
le2sky <leehaneul990623@gmail.com>
2024-11-14 20:41:20 +0900

    chore: 미사용 메서드 제거

--- src/main/java/maeilmail/mail/MailEventRepository.java
+++ src/main/java/maeilmail/mail/MailEventRepository.java
@@ -1,10 +1,6 @@
 package maeilmail.mail;
 
-import java.time.LocalDateTime;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MailEventRepository extends JpaRepository<MailEvent, Long> {
-
-    List<MailEvent> findMailEventByCreatedAtBetween(LocalDateTime startOfDay, LocalDateTime endOfDay);
 }
