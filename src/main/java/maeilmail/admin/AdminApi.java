5e2804f30afa2a5a13bc3bf5269de90ff3aa9032
kunsanglee <gosmdochee2@gmail.com>
2024-11-14 17:10:11 +0900

    feat: 관리자가 모든 구독자에게 공지 메일을 발송하는 기능 구현

--- src/main/java/maeilmail/admin/AdminApi.java
+++ src/main/java/maeilmail/admin/AdminApi.java
@@ -3,6 +3,7 @@ package maeilmail.admin;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 class AdminApi {
 
     private final AdminQuestionService adminQuestionService;
+    private final AdminNoticeService adminNoticeService;
 
     @PutMapping("/admin/question")
     public ResponseEntity<Void> putQuestion(@RequestBody AdminQuestionRequest request) {
@@ -19,6 +21,13 @@ class AdminApi {
         } else {
             adminQuestionService.createQuestion(request.toQuestion());
         }
+
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/admin/notice")
+    public ResponseEntity<Void> sendNotice(@RequestBody AdminNoticeRequest request) {
+        adminNoticeService.sendNotice(request);
 
         return ResponseEntity.noContent().build();
     }
---
+++ src/main/java/maeilmail/admin/AdminNoticeService.java
@@ -0,0 +1,37 @@
+package maeilmail.admin;
+
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import maeilmail.mail.MailMessage;
+import maeilmail.mail.MailSender;
+import maeilmail.subscribe.SubscribeRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class AdminNoticeService {
+
+    private final MailSender mailSender;
+    private final SubscribeRepository subscribeRepository;
+
+    @Transactional
+    public void sendNotice(AdminNoticeRequest request) {
+        log.info("공지 전송을 시작합니다.");
+        List<String> distinctEmails = subscribeRepository.findDistinctEmails();
+
+        log.info("{}명의 구독자에게 공지 메일을 발송합니다.", distinctEmails.size());
+
+        distinctEmails.stream()
+                .map(it -> createNotice(it, request.title(), request.content()))
+                .forEach(mailSender::sendMail);
+
+        log.info("공지 전송을 종료합니다.");
+    }
+
+    private MailMessage createNotice(String email, String title, String content) {
+        return new MailMessage(email, title, content, "notice");
+    }
+}
