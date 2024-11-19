ceb0cd14bad43bc9f963f0d40cfe016e3d625a6c
le2sky <leehaneul990623@gmail.com>
2024-11-19 15:29:14 +0900

    test: QuestionSender 모킹 처리

--- src/test/java/maeilmail/support/IntegrationTestSupport.java
+++ src/test/java/maeilmail/support/IntegrationTestSupport.java
@@ -5,10 +5,14 @@ import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 
 import maeilmail.mail.MailSender;
+import maeilmail.subscribequestion.QuestionSender;
 import org.hibernate.cfg.AvailableSettings;
+import org.junit.jupiter.api.AfterEach;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +21,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 @Import(IntegrationTestSupport.TestConfig.class)
 public abstract class IntegrationTestSupport {
+
+    @Autowired
+    private CacheManager cacheManager;
+
+    @AfterEach
+    void tearDown() {
+        cacheManager.getCacheNames().forEach(name -> cacheManager.getCache(name).clear());
+    }
 
     @TestConfiguration
     public static class TestConfig {
@@ -29,6 +41,16 @@ public abstract class IntegrationTestSupport {
                     .sendMail(any());
 
             return mailSender;
+        }
+
+        @Bean
+        public QuestionSender questionSender() {
+            QuestionSender questionSender = mock(QuestionSender.class);
+            doNothing()
+                    .when(questionSender)
+                    .sendMail(any());
+
+            return questionSender;
         }
 
         @Bean
