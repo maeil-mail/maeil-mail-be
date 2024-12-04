6aaa3aa53a6cd4183f3e2f865bb95415557d6bd8
kunsanglee <gosmdochee2@gmail.com>
2024-12-04 13:01:00 +0900

    refactor: MailSender가 MailMimeMessageCreator 사용하도록 변경

--- src/main/java/maeilmail/mail/MailSender.java
+++ src/main/java/maeilmail/mail/MailSender.java
@@ -1,37 +1,23 @@
 package maeilmail.mail;
 
-import jakarta.mail.MessagingException;
-import jakarta.mail.internet.MimeMessage;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component("emailSender")
-public class MailSender extends AbstractMailSender<MailMessage> {
+public class MailSender extends AbstractMailSender<MailMessage, MailMimeMessageCreator> {
 
     private final MailEventRepository mailEventRepository;
 
-    public MailSender(JavaMailSender javaMailSender, MailEventRepository mailEventRepository) {
-        super(javaMailSender);
+    public MailSender(JavaMailSender javaMailSender, MailMimeMessageCreator mimeMessageCreator, MailEventRepository mailEventRepository) {
+        super(javaMailSender, mimeMessageCreator);
         this.mailEventRepository = mailEventRepository;
     }
 
     @Override
     protected void logSending(MailMessage message) {
         log.info("메일을 전송합니다. email = {} subject = {} type = {}", message.to(), message.subject(), message.type());
-    }
-
-    @Override
-    protected MimeMessage createMimeMessage(MailMessage message) throws MessagingException {
-        MimeMessage mimeMessage = javaMailSender.createMimeMessage();
-        MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, false, "UTF-8");
-        helper.setFrom(FROM_EMAIL);
-        helper.setTo(message.to());
-        helper.setSubject("[매일메일] " + message.subject());
-        helper.setText(message.text(), true);
-        return mimeMessage;
     }
 
     @Override
