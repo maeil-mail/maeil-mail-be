0f9e94de53dc509b659309e03eb134c405c807ce
le2sky <leehaneul990623@gmail.com>
2024-12-22 12:22:18 +0900

    feat: 등록 공지 조회 기능 구현

--- src/main/java/maeilmail/admin/AdminApi.java
+++ src/main/java/maeilmail/admin/AdminApi.java
@@ -1,8 +1,10 @@
 package maeilmail.admin;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -12,6 +14,7 @@ class AdminApi {
 
     private final AdminQuestionService adminQuestionService;
     private final AdminNoticeService adminNoticeService;
+    private final AdminNoticeRepository adminNoticeRepository;
 
     @PutMapping("/admin/question")
     public ResponseEntity<Void> putQuestion(@RequestBody AdminQuestionRequest request) {
@@ -33,5 +36,12 @@ class AdminApi {
         }
 
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping("/admin/notice")
+    public ResponseEntity<List<AdminNoticeResponse>> getNotice() {
+        List<AdminNoticeResponse> responses = adminNoticeRepository.queryAll();
+
+        return ResponseEntity.ok(responses);
     }
 }
