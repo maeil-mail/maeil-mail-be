e9aebccd2c2d48354fa41a4b161fe05371b3adae
le2sky <leehaneul990623@gmail.com>
2025-01-02 21:34:36 +0900

    fix: 어드민 보고 스케줄러에서 신규 기능 사용하도록 변경

--- src/main/java/maeilmail/admin/AdminReportScheduler.java
+++ src/main/java/maeilmail/admin/AdminReportScheduler.java
@@ -6,7 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import maeilmail.mail.MailMessage;
 import maeilmail.mail.MailSender;
-import maeilmail.statistics.EventReport;
+import maeilmail.statistics.DailySendReport;
 import maeilmail.statistics.StatisticsService;
 import maeilmail.support.DistributedSupport;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -17,7 +17,7 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 class AdminReportScheduler {
 
-    private static final String REPORT_FORMAT = "질문 전송 카운트(성공/실패) : %d/%d";
+    private static final String REPORT_FORMAT = "질문 전송 카운트(전송 대상 건수/실제 전송 대상 건수/성공/실패) : %d/%d/%d/%d";
 
     private final MailSender mailSender;
     private final AdminRepository adminRepository;
@@ -27,9 +27,11 @@ class AdminReportScheduler {
 
     @Scheduled(cron = "0 30 7 * * MON-FRI", zone = "Asia/Seoul")
     public void sendReport() {
-        log.info("관리자 결과 전송, date = {}", LocalDate.now());
-        EventReport report = statisticsService.generateDailySubscribeQuestionReport();
-        String text = createText(report);
+        LocalDate today = LocalDate.now();
+        log.info("관리자 결과 전송, date = {}", today);
+        DailySendReport dailySendReport = statisticsService.generateDailySendReport(today);
+
+        String text = createText(dailySendReport);
         String subject = "관리자 전용 메일 전송 결과를 알려드립니다.";
 
         adminRepository.findAll().stream()
@@ -38,8 +40,14 @@ class AdminReportScheduler {
                 .forEach(mailSender::sendMail);
     }
 
-    private String createText(EventReport report) {
-        String reportText = String.format(REPORT_FORMAT, report.success(), report.fail());
+    private String createText(DailySendReport report) {
+        String reportText = String.format(
+                REPORT_FORMAT,
+                report.expectedSendingCount(),
+                report.actualSendingCount(),
+                report.success(),
+                report.fail()
+        );
 
         return adminReportView.render(Map.of("report", reportText));
     }
