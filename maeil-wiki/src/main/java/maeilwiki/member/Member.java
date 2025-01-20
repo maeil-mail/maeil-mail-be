ca90b78f7573bb24f3ce8e60b72c784cead7635c
Nam Gi Beom <rlqja0523@naver.com>
2025-01-20 18:08:13 +0900

    feat: Member 엔티티 추가

---
+++ maeil-wiki/src/main/java/maeilwiki/member/Member.java
@@ -0,0 +1,58 @@
+package maeilwiki.member;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import maeilsupport.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Member extends BaseEntity {
+
+    private static final int MAX_NAME_LENGTH = 255;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true, length = MAX_NAME_LENGTH)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String providerId;
+
+    @Column(nullable = false, columnDefinition = "VARCHAR(10)")
+    @Enumerated(value = EnumType.STRING)
+    private Provider provider;
+
+    private String profileImageUrl;
+
+    private LocalDateTime deletedAt;
+
+    public Member(String name, String providerId, String provider) {
+        validateName(name);
+        this.name = name;
+        this.providerId = providerId;
+        this.provider = Provider.from(provider);
+    }
+
+    private void validateName(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("이름은 필수 입력값입니다.");
+        }
+
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("이름은 %d자 이하여야 합니다.".formatted(MAX_NAME_LENGTH));
+        }
+    }
+}
