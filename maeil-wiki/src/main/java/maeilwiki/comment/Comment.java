7067e04979af12b83faf3e7a833f0e4f4e6484da
Nam Gi Beom <rlqja0523@naver.com>
2025-01-20 18:09:09 +0900

    feat: Comment 엔티티 추가

---
+++ maeil-wiki/src/main/java/maeilwiki/comment/Comment.java
@@ -0,0 +1,58 @@
+package maeilwiki.comment;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import maeilsupport.BaseEntity;
+import maeilwiki.member.Member;
+import maeilwiki.wiki.Wiki;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String answer;
+
+    @Column(nullable = false)
+    private boolean isAnonymous;
+
+    private LocalDateTime deletedAt;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(nullable = false)
+    private Wiki wiki;
+
+    public Comment(String answer, boolean isAnonymous, Member member, Wiki wiki) {
+        validateAnswer(answer);
+        this.answer = answer;
+        this.isAnonymous = isAnonymous;
+        this.member = member;
+        this.wiki = wiki;
+    }
+
+    private void validateAnswer(String answer) {
+        if (answer == null || answer.isBlank()) {
+            throw new IllegalArgumentException("답변은 필수 입력값입니다.");
+        }
+    }
+}
