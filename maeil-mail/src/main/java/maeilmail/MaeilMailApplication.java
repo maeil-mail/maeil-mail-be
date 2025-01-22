49e13a811d3383af1953c074df75ac3bbab15443
Nam Gi Beom <rlqja0523@naver.com>
2025-01-23 05:29:12 +0900

    refactor: wiki configuration 삭제

--- maeil-mail/src/main/java/maeilmail/MaeilMailApplication.java
+++ maeil-mail/src/main/java/maeilmail/MaeilMailApplication.java
@@ -2,10 +2,14 @@ package maeilmail;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
 @SpringBootApplication(scanBasePackages = {"maeilmail", "maeilwiki", "maeilsupport"})
+@EntityScan(basePackages = {"maeilmail", "maeilwiki"})
+@EnableJpaRepositories(basePackages = {"maeilmail", "maeilwiki"})
 public class MaeilMailApplication {
 
     public static void main(String[] args) {
--- maeil-wiki/src/main/java/maeilwiki/WikiConfiguration.java
+++
@@ -1,9 +0,0 @@
-package maeilwiki;
-
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableAutoConfiguration
-public class WikiConfiguration {
-}
