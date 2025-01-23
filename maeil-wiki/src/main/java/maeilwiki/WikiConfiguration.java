e6dc7241cc54ee1bcceb931f430b4f616054ed42
le2sky <leehaneul990623@gmail.com>
2025-01-23 12:35:53 +0900

    chore: 토큰 설정을 위한 spring properties 사전 작업

--- maeil-wiki/src/main/java/maeilwiki/WikiConfiguration.java
+++ maeil-wiki/src/main/java/maeilwiki/WikiConfiguration.java
@@ -1,11 +1,13 @@
 package maeilwiki;
 
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+@ConfigurationPropertiesScan
 public class WikiConfiguration {
 }
