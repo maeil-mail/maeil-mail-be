c5ddbbf155764879f7250239c5589049e895ae08
Nam Gi Beom <rlqja0523@naver.com>
2025-01-28 19:56:24 +0900

    feat: 테스트 격리 extension 추가

--- maeil-wiki/src/test/java/maeilwiki/support/IntegrationTestSupport.java
+++ maeil-wiki/src/test/java/maeilwiki/support/IntegrationTestSupport.java
@@ -7,13 +7,14 @@ import static org.springframework.boot.test.context.SpringBootTest.WebEnvironmen
 
 import maeilwiki.member.github.GithubClient;
 import maeilwiki.member.github.GithubMember;
+import maeilwiki.support.extension.DatabaseCleanerExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
+@ExtendWith(DatabaseCleanerExtension.class)
 @SpringBootTest(webEnvironment = WebEnvironment.NONE)
 @Import(IntegrationTestSupport.TestConfig.class)
 public abstract class IntegrationTestSupport {
