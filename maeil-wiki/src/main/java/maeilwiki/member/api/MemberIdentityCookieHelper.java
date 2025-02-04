20546f47b20c222b56a34f748c46c1a9830bcbdb
le2sky <leehaneul990623@gmail.com>
2025-02-04 23:40:20 +0900

    feat: member cookie 헬퍼 구현

---
+++ maeil-wiki/src/main/java/maeilwiki/member/api/MemberIdentityCookieHelper.java
@@ -0,0 +1,61 @@
+package maeilwiki.member.api;
+
+import static maeilwiki.member.api.MemberIdentityCookieType.ACCESS_TOKEN;
+import static maeilwiki.member.api.MemberIdentityCookieType.REFRESH_TOKEN;
+
+import jakarta.servlet.http.Cookie;
+import lombok.RequiredArgsConstructor;
+import maeilwiki.member.infra.MemberTokenProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+class MemberIdentityCookieHelper {
+
+    private final MemberTokenProperties properties;
+
+    @Value("${domain}")
+    private String serverDomain;
+
+    public String generateAccessTokenCookie(String value) {
+        return ResponseCookie.from(ACCESS_TOKEN.getName(), value)
+                .domain(serverDomain)
+                .path("/")
+                .maxAge(properties.accessExpTime())
+                .sameSite("none")
+                .httpOnly(true)
+                .secure(true)
+                .build()
+                .toString();
+    }
+
+    public String generateRefreshTokenCookie(String value, String path) {
+        return ResponseCookie.from(REFRESH_TOKEN.getName(), value)
+                .domain(serverDomain)
+                .path(path)
+                .maxAge(properties.refreshExpTime())
+                .sameSite("none")
+                .httpOnly(true)
+                .secure(true)
+                .build()
+                .toString();
+    }
+
+    public String getCookieByName(Cookie[] cookies, MemberIdentityCookieType cookieType) {
+        if (cookies == null || cookies.length == 0) {
+            return null;
+        }
+
+        String target = null;
+        for (Cookie cookie : cookies) {
+            String name = cookie.getName();
+            if (name.equals(cookieType.getName())) {
+                target = cookie.getValue();
+            }
+        }
+
+        return target;
+    }
+}
---
+++ maeil-wiki/src/test/java/maeilwiki/member/api/MemberIdentityCookieHelperTest.java
@@ -0,0 +1,59 @@
+package maeilwiki.member.api;
+
+import static maeilwiki.member.api.MemberIdentityCookieType.ACCESS_TOKEN;
+import static maeilwiki.member.api.MemberIdentityCookieType.REFRESH_TOKEN;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+import jakarta.servlet.http.Cookie;
+import maeilwiki.support.IntegrationTestSupport;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class MemberIdentityCookieHelperTest extends IntegrationTestSupport {
+
+    @Autowired
+    private MemberIdentityCookieHelper helper;
+
+    @Test
+    @DisplayName("주어진 쿠키 목록에서 원하는 쿠키를 조회할 수 있다.")
+    void getCookieById() {
+        String expectedValue = "1234";
+        Cookie accessCookie = new Cookie(ACCESS_TOKEN.getName(), expectedValue);
+        Cookie refreshCookie = new Cookie(REFRESH_TOKEN.getName(), "1234");
+
+        Cookie[] cookies = {accessCookie, refreshCookie};
+
+        String result = helper.getCookieByName(cookies, ACCESS_TOKEN);
+
+        assertThat(result).isEqualTo(expectedValue);
+    }
+
+    @Test
+    @DisplayName("쿠키를 찾을 수 없는 경우, null을 반환한다.")
+    void cookieNotFound() {
+        Cookie accessCookie = new Cookie(ACCESS_TOKEN.getName(), "1234");
+        Cookie[] cookies = {accessCookie};
+
+        String result = helper.getCookieByName(cookies, REFRESH_TOKEN);
+
+        assertThat(result).isNull();
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "generateInvalidCookies")
+    @DisplayName("쿠키 배열이 비정상 케이스인 경우, null을 반환한다.")
+    void invalidCookies(Cookie[] cookies) {
+        String result = helper.getCookieByName(cookies, ACCESS_TOKEN);
+
+        assertThat(result).isNull();
+    }
+
+    public static Stream<Arguments> generateInvalidCookies() {
+        return Stream.of(Arguments.of((Object) new Cookie[]{}), Arguments.of((Object) null));
+    }
+}
