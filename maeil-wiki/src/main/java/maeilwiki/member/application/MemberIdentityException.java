24885e7a8b25f887b9c0ebd14112089caa2a6c75
le2sky <leehaneul990623@gmail.com>
2025-02-12 13:49:49 +0900

    fix: MemberIdentityException isExpired 필드 추가

--- maeil-wiki/src/main/java/maeilwiki/member/application/MemberIdentityException.java
+++ maeil-wiki/src/main/java/maeilwiki/member/application/MemberIdentityException.java
@@ -4,11 +4,24 @@ public class MemberIdentityException extends RuntimeException {
 
     private static final String IDENTITY_EXCEPTION_MESSAGE = "유효한 토큰이 존재하지 않습니다.";
 
+    private final boolean isExpired;
+
     public MemberIdentityException() {
         super(IDENTITY_EXCEPTION_MESSAGE);
+        this.isExpired = false;
     }
 
     public MemberIdentityException(Throwable cause) {
         super(IDENTITY_EXCEPTION_MESSAGE, cause);
+        this.isExpired = false;
+    }
+
+    public MemberIdentityException(Throwable cause, boolean isExpired) {
+        super(IDENTITY_EXCEPTION_MESSAGE, cause);
+        this.isExpired = isExpired;
+    }
+
+    public boolean isExpired() {
+        return isExpired;
     }
 }
