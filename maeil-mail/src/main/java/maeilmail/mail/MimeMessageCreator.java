3ce41bf6385964295e7c7207344a435d6e5ef7c4
Nam Gi Beom <rlqja0523@naver.com>
2025-02-16 19:11:24 +0900

    refactor: 메일 발신 주소 변경

--- maeil-mail/src/main/java/maeilmail/mail/MimeMessageCreator.java
+++ maeil-mail/src/main/java/maeilmail/mail/MimeMessageCreator.java
@@ -5,7 +5,7 @@ import jakarta.mail.internet.MimeMessage;
 
 public abstract class MimeMessageCreator<T> {
 
-    protected static final String FROM_EMAIL = "maeil-mail <maeil-mail-noreply@maeil-mail.site>";
+    protected static final String FROM_EMAIL = "maeil-mail <noreply@maeil-mail.kr>";
     protected static final String TITLE_PREFIX = "[매일메일] %s";
 
     public abstract MimeMessage createMimeMessage(MimeMessage mimeMessage, T message) throws MessagingException;
