d0ee1b053a7525ea24cb37f853bfe37d3ce6a85a
Haneul Lee <leehaneul990623@gmail.com>
2025-03-31 00:00:27 +0900

    style: 공백 제거

--- maeil-wiki/src/main/java/maeilwiki/mutiplechoice/domain/Options.java
+++ maeil-wiki/src/main/java/maeilwiki/mutiplechoice/domain/Options.java
@@ -8,12 +8,11 @@ public class Options {
 
     public Options(List<Option> options) {
         validateSize(options);
-
         this.options = options;
     }
 
     private void validateSize(List<Option> options) {
-        if(options.isEmpty()) {
+        if (options.isEmpty()) {
             throw new IllegalArgumentException("객관식 항목은 최소 1개 이상이어야 합니다.");
         }
     }
