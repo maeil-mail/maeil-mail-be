01a341639d6d1cae1bf85838a51dc2cc0398ba4e
Haneul Lee <leehaneul990623@gmail.com>
2025-04-01 00:39:54 +0900

    feat: options 정답 수 검증 로직 추가

--- maeil-wiki/src/main/java/maeilwiki/mutiplechoice/domain/Options.java
+++ maeil-wiki/src/main/java/maeilwiki/mutiplechoice/domain/Options.java
@@ -6,11 +6,19 @@ public record Options(List<Option> options) {
 
     public Options {
         validateSize(options);
+        validateAnswerCount(options);
     }
 
     private void validateSize(List<Option> options) {
         if (options.isEmpty()) {
             throw new IllegalArgumentException("객관식 항목은 최소 1개 이상이어야 합니다.");
         }
+    }
+
+    private void validateAnswerCount(List<Option> options) {
+        options.stream()
+                .filter(Option::isCorrectAnswer)
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("객관식 항목의 정답은 최소 1개 이상이어야 합니다."));
     }
 }
