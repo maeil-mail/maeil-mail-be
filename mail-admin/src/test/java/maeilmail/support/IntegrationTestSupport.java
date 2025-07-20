c9a6620d5fbc33426e98d16fb03d807a0b51b7bd
Haneul Lee <leehaneul990623@gmail.com>
2025-07-20 12:01:20 +0900

    test: 어드민 미사용 의존성 제거

--- mail-admin/src/test/java/maeilmail/support/IntegrationTestSupport.java
+++ mail-admin/src/test/java/maeilmail/support/IntegrationTestSupport.java
@@ -1,6 +1,9 @@
 package maeilmail.support;
 
+import maeilmail.bulksend.sender.QuestionSender;
+import maeilmail.bulksend.sender.WeeklyQuestionSender;
 import maeilmail.mail.MailSender;
+import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,4 +14,10 @@ public abstract class IntegrationTestSupport {
 
     @MockBean
     protected MailSender mailSender;
+
+    @MockBean
+    protected QuestionSender questionSender;
+
+    @MockBean
+    protected WeeklyQuestionSender weeklyQuestionSender;
 }
