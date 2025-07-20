09786747a7bb760927888dd7804af409560dc602
Haneul Lee <leehaneul990623@gmail.com>
2025-07-20 12:22:19 +0900

    chore: 미사용 의존성 제거

--- mail-admin/src/test/java/maeilmail/support/IntegrationTestSupport.java
+++ mail-admin/src/test/java/maeilmail/support/IntegrationTestSupport.java
@@ -3,7 +3,6 @@ package maeilmail.support;
 import maeilmail.bulksend.sender.QuestionSender;
 import maeilmail.bulksend.sender.WeeklyQuestionSender;
 import maeilmail.mail.MailSender;
-import org.mockito.Mock;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.transaction.annotation.Transactional;
