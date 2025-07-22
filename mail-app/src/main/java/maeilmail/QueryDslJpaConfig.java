e50e0f0efac3ea4a664d92626c760797001210f4
Haneul Lee <leehaneul990623@gmail.com>
2025-07-22 21:30:35 +0900

    refactor: querydsl config 클래스 이름 변경

--- mail-app/src/main/java/maeilmail/QueryDslJpaConfig.java
+++ mail-app/src/main/java/maeilmail/QueryDslJpaConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @RequiredArgsConstructor
-class QueryDslConfig {
+class QueryDslJpaConfig {
 
     private final EntityManager entityManager;
 
