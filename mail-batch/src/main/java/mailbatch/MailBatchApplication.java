c1a1df0307ea2c202fb156f0965dd4acbf2011e6
Haneul Lee <leehaneul990623@gmail.com>
2025-08-14 10:53:54 +0900

    chore: 배치 의존성 추가

---
+++ mail-batch/src/main/java/mailbatch/MailBatchApplication.java
@@ -0,0 +1,12 @@
+package mailbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class MailBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MailBatchApplication.class, args);
+    }
+}
