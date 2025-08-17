4651c4f40cfe2c7bd8c3d15f8ce89e2fd98249b4
Haneul Lee <leehaneul990623@gmail.com>
2025-08-17 11:41:26 +0900

    refactor: 배치 컴토넌트 분리

---
+++ mail-batch/src/main/java/maeilbatch/processor/DailySubscribeProcessor.java
@@ -0,0 +1,48 @@
+package maeilbatch.processor;
+
+import java.util.HashMap;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import maeilmail.bulksend.sender.ChoiceQuestionPolicy;
+import maeilmail.bulksend.sender.SubscribeQuestionMessage;
+import maeilmail.bulksend.view.SubscribeQuestionView;
+import maeilmail.question.QuestionSummary;
+import maeilmail.subscribe.command.domain.Subscribe;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DailySubscribeProcessor implements ItemProcessor<Subscribe, SubscribeQuestionMessage> {
+
+    private final ChoiceQuestionPolicy choiceQuestionPolicy;
+    private final SubscribeQuestionView subscribeQuestionView;
+
+    @Override
+    public SubscribeQuestionMessage process(Subscribe subscribe) {
+        try {
+            QuestionSummary questionSummary = choiceQuestionPolicy.choice(subscribe);
+            String subject = createSubject(questionSummary);
+            String text = createText(subscribe, questionSummary);
+            return new SubscribeQuestionMessage(subscribe, questionSummary.toQuestion(), subject, text);
+        } catch (Exception e) {
+            log.error("일간 면접 질문 선택 실패. 구독자 id = {}", subscribe.getId(), e);
+            return null;
+        }
+    }
+
+    private String createSubject(QuestionSummary question) {
+        return question.title();
+    }
+
+    private String createText(Subscribe subscribe, QuestionSummary question) {
+        HashMap<Object, Object> attribute = new HashMap<>();
+        attribute.put("questionId", question.id());
+        attribute.put("question", question.title());
+        attribute.put("email", subscribe.getEmail());
+        attribute.put("token", subscribe.getToken());
+
+        return subscribeQuestionView.render(attribute);
+    }
+}
