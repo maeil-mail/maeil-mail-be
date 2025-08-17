2e51683f54ffbc1858ff8790c7b3b09f68e2b342
Haneul Lee <leehaneul990623@gmail.com>
2025-08-17 11:41:26 +0900

    refactor: 배치 컴토넌트 분리

---
+++ mail-batch/src/main/java/maeilbatch/reader/SubscribeItemReaderGenerator.java
@@ -0,0 +1,39 @@
+package maeilbatch.reader;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+import jakarta.persistence.EntityManagerFactory;
+import lombok.RequiredArgsConstructor;
+import maeilmail.subscribe.command.domain.Subscribe;
+import maeilmail.subscribe.command.domain.SubscribeFrequency;
+import org.springframework.batch.item.database.JpaCursorItemReader;
+import org.springframework.batch.item.database.builder.JpaCursorItemReaderBuilder;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SubscribeItemReaderGenerator {
+
+    private final EntityManagerFactory entityManagerFactory;
+
+    public JpaCursorItemReader<Subscribe> generate(SubscribeFrequency frequency, LocalDateTime datetime) {
+        return new JpaCursorItemReaderBuilder<Subscribe>()
+                .name("dailySubscribeReader")
+                .entityManagerFactory(entityManagerFactory)
+                .queryString("""
+                           select s
+                           from Subscribe s
+                           where
+                               s.createdAt <= :createdAt and
+                               s.deletedAt is null and
+                               s.frequency = :frequency
+                           order by s.id ASC
+                        """)
+                .parameterValues(Map.of(
+                        "createdAt", datetime,
+                        "frequency", frequency.toLowerCase()
+                ))
+                .hintValues(Map.of("org.hibernate.fetchSize", 100))
+                .build();
+    }
+}
---
+++ mail-batch/src/main/java/maeilbatch/writer/DailyMailSendWriter.java
@@ -0,0 +1,22 @@
+package maeilbatch.writer;
+
+import lombok.RequiredArgsConstructor;
+import maeilmail.bulksend.sender.QuestionSender;
+import maeilmail.bulksend.sender.SubscribeQuestionMessage;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class DailyMailSendWriter implements ItemWriter<SubscribeQuestionMessage> {
+
+    private final QuestionSender questionSender;
+
+    @Override
+    public void write(Chunk<? extends SubscribeQuestionMessage> chunk) {
+        for (SubscribeQuestionMessage message : chunk) {
+            questionSender.sendMail(message);
+        }
+    }
+}
