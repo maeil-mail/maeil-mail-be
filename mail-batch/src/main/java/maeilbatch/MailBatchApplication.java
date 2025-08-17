ab8d32f36938232b8ef6e906c4ae4635b65c5321
Haneul Lee <leehaneul990623@gmail.com>
2025-08-17 10:51:35 +0900

    fix: 일간 메일 발송 배치 실행 구성

--- mail-batch/src/main/java/maeilbatch/MailBatchApplication.java
+++ mail-batch/src/main/java/maeilbatch/MailBatchApplication.java
@@ -2,12 +2,18 @@ package maeilbatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @ConfigurationPropertiesScan(basePackages = {"maeilmail", "maeilbatch"})
 @SpringBootApplication(
         scanBasePackages = {"maeilmail", "maeilbatch"}
 )
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = {"maeilmail", "maeilbatch"})
+@EntityScan(basePackages = {"maeilmail", "maeilbatch"})
 class MailBatchApplication {
 
     public static void main(String[] args) {
