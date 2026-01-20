3d7c4299cd691b763fdd2961b8d60b4156f05a0a
bricksky <skydh1214@naver.com>
2026-01-20 15:13:34 +0900

    refactor: 회원 요청의 유효성 메시지 제거

--- wiki-core/src/main/java/maeilwiki/member/application/MemberRequest.java
+++ wiki-core/src/main/java/maeilwiki/member/application/MemberRequest.java
@@ -3,6 +3,5 @@ package maeilwiki.member.application;
 import jakarta.validation.constraints.NotBlank;
 
 public record MemberRequest(
-        @NotBlank(message = "OAuth Access Token은 필수입니다.")
         String oauthAccessToken) {
 }
