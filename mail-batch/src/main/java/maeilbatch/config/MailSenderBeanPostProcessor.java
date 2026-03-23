b867e4447b36147d828912dec8e7ebb0bdbf0dd2
Haneul Lee <leehaneul990623@gmail.com>
2026-03-23 22:16:49 +0900

    refactor: 빈 후처리기로 개선
    - 설정값 분리 + 중복 delegate 필드 제거를 위함

---
+++ mail-batch/src/main/java/maeilbatch/config/MailSenderBeanPostProcessor.java
@@ -0,0 +1,32 @@
+package maeilbatch.config;
+
+import maeilbatch.SmtpConnectionPool;
+import maeilbatch.SmtpConnectionPoolProxy;
+import maeilbatch.SmtpConnectionProperties;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+public class MailSenderBeanPostProcessor implements BeanPostProcessor, DisposableBean {
+
+    private SmtpConnectionPoolProxy pooledMailSender;
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        if (!"mailSender".equals(beanName) || !(bean instanceof JavaMailSenderImpl delegate)) {
+            return bean;
+        }
+
+        SmtpConnectionProperties settings = SmtpConnectionProperties.from(delegate);
+        this.pooledMailSender = new SmtpConnectionPoolProxy(delegate, new SmtpConnectionPool(settings));
+        return pooledMailSender;
+    }
+
+    @Override
+    public void destroy() {
+        if (pooledMailSender != null) {
+            pooledMailSender.close();
+        }
+    }
+}
---
+++ mail-batch/src/main/java/maeilbatch/config/MailSenderConfig.java
@@ -0,0 +1,14 @@
+package maeilbatch.config;
+
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MailSenderConfig {
+
+    @Bean
+    public BeanPostProcessor connectionPoolJavaMailSenderBeanPostProcessor() {
+        return new MailSenderBeanPostProcessor();
+    }
+}
