c2156bdee51fba19110972708e12b05b1e4bcb25
Haneul Lee <leehaneul990623@gmail.com>
2026-03-28 16:36:39 +0900

    fix: 미사용 클래스 제거

--- mail-admin/src/test/java/maeilmail/support/IntegrationTestSupport.java
+++ mail-admin/src/test/java/maeilmail/support/IntegrationTestSupport.java
@@ -3,14 +3,12 @@ package maeilmail.support;
 import com.querydsl.jpa.JPQLTemplates;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import jakarta.persistence.EntityManager;
-import maeilmail.bulksend.sender.QuestionSender;
-import maeilmail.bulksend.sender.WeeklyQuestionSender;
 import maeilmail.mail.MailSender;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -18,14 +16,8 @@ import org.springframework.transaction.annotation.Transactional;
 @Import(IntegrationTestSupport.TestConfig.class)
 public abstract class IntegrationTestSupport {
 
-    @MockBean
+    @MockitoBean
     protected MailSender mailSender;
-
-    @MockBean
-    protected QuestionSender questionSender;
-
-    @MockBean
-    protected WeeklyQuestionSender weeklyQuestionSender;
 
     @TestConfiguration
     public static class TestConfig {
